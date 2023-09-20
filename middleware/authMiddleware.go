00de11c96ad64e2ef2d2bef5b21a69e977cd65f1
aviel-lanzman <avielnzmn@gmail.com>
2023-09-20 17:54:16 +0300

    fix(authMiddleware.go): refactor AuthMiddleware to separate GetUserFromCookiesByJWT and InsertUserToSession functions for better code organization and reusability fix(superUserMiddleware.go): refactor SuperUserMiddleware to use GetUserFromCookiesByJWT and InsertUserToSession functions for better code organization and reusability

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -2,31 +2,49 @@ package middleware
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/bokoness/lashon/models"
 	"github.com/bokoness/lashon/services"
 	"github.com/gofiber/fiber/v2"
 )
 
-func AuthMiddleware(c *fiber.Ctx) error {
-	cookie := c.Cookies("user")
-
-	if cookie == "" {
-		return c.SendStatus(fiber.StatusUnauthorized)
-	}
-
-	user, err := services.GetUserByJWT(cookie)
-
-	// insert user to session
+func InsertUserToSession(c *fiber.Ctx, user *models.User) int {
 	sess, err := services.GetStore(c)
-
+	statusCodeError := fiber.StatusInternalServerError
 	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError)
+		return statusCodeError
 	}
 
 	sess.Set("user", user)
 
 	if err = sess.Save(); err != nil {
-		fmt.Printf("this is the error %s", err)
-		return fiber.NewError(fiber.StatusInternalServerError)
+		log.Fatal(fmt.Printf("this is the error %s", err))
+		return statusCodeError
+	}
+
+	return 0
+}
+func GetUserFromCookiesByJWT(c *fiber.Ctx) (*models.User, error) {
+	cookie := c.Cookies("user")
+
+	if cookie == "" {
+		return nil, fiber.NewError(fiber.StatusUnauthorized)
+	}
+
+	user, err := services.GetUserByJWT(cookie)
+	return user, err
+}
+func AuthMiddleware(c *fiber.Ctx) error {
+	user, err := GetUserFromCookiesByJWT(c)
+	if err != nil || user == nil {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// insert user to session
+	statusCodeError := InsertUserToSession(c, user)
+	if statusCodeError != 0 {
+		return fiber.NewError(statusCodeError)
 	}
 
 	if err != nil {
--- middleware/superUserMiddleware.go
+++ middleware/superUserMiddleware.go
@@ -1,42 +1,27 @@
 package middleware
 
 import (
-	"fmt"
-
-	"github.com/bokoness/lashon/services"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SuperUserMiddleware(c *fiber.Ctx) error {
-	cookie := c.Cookies("user")
-
-	if cookie == "" {
-		return c.SendStatus(fiber.StatusUnauthorized)
+	user, err := GetUserFromCookiesByJWT(c)
+	if err != nil || user == nil {
+		return err
 	}
-
-	user, err := services.GetUserByJWT(cookie)
 
 	if !user.IsSuperuser {
 		return c.SendStatus(fiber.StatusUnauthorized)
 	}
 
 	// insert user to session
-	sess, err := services.GetStore(c)
-
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError)
-	}
-
-	sess.Set("user", user)
-
-	if err = sess.Save(); err != nil {
-		fmt.Printf("this is the error %s", err)
-		return fiber.NewError(fiber.StatusInternalServerError)
+	statusCodeError := InsertUserToSession(c, user)
+	if statusCodeError != 0 {
+		return fiber.NewError(statusCodeError)
 	}
 
 	if err != nil {
 		return c.SendStatus(fiber.StatusUnauthorized)
 	}
-
 	return c.Next()
 }
