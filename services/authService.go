9d05f34c4e708d8ba98c54f8de5d5a49a634fe6d
bokoness <bokoness@gmail.com>
2023-09-22 07:57:25 +0300

    style(auth): lowercase for error

--- services/authService.go
+++ services/authService.go
@@ -33,7 +33,7 @@ func GenerateAuthJWT(email string) (string, error) {
 func UnHashAuthJWT(hash string) (interface{}, error) {
 	token, err := jwt.Parse(hash, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(jwtSecret), nil
 	})
