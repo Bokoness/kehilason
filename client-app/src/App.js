b369ee5caa20a2781ff8313e212e780cc548feb9
Yehoshua Tarshish <a0526741852@gmail.com>
2023-09-29 09:58:23 +0300

    login page

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,22 +1,27 @@
-import './App.css';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import RegisterPage from "./views/Register"
-import {Container} from "@mui/material";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegisterPage from "./pages/Register.jsx";
+import { Container } from "@mui/material";
+import Login from "./pages/Login.jsx";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <RegisterPage/>
-        }
-    ])
-    return (
-        <div className="App">
-            <Container style={{paddingTop: 5}}>
-                <RouterProvider router={router}/>
-            </Container>
-        </div>
-    );
+  const router = createBrowserRouter([
+    {
+      path: "/signup",
+      element: <RegisterPage />,
+    },
+    {
+      path: "/",
+      element: <Login />,
+    },
+  ]);
+  return (
+    <div className="App">
+      <Container style={{ paddingTop: 5 }}>
+        <RouterProvider router={router} />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
