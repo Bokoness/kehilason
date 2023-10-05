c7fe280096c653711cf94a07e69369cfce998301
Aviel Lanzman <avielnzmn@gmail.com>
2023-10-05 08:15:42 +0300

    wip

---
+++ client-app/src/components/comunnityCard.jsx
@@ -0,0 +1,21 @@
+import { Card ,IconButton,CardHeader,
+Avatar} from '@mui/material';
+
+export default ComunnityCard=()=>{
+
+return<CardHeader
+  avatar={
+    <Avatar aria-label="">
+      
+    </Avatar>
+  }
+  action={
+    <IconButton aria-label="">
+      
+    </IconButton>
+  }
+  title=""
+  subheader=""
+  
+/>
+}
