e2f99cd23d7530df4bfee05a9dc997d08c035088
bokoness <bokoness@gmail.com>
2023-11-09 23:26:46 +0200

    style(client-core): add custom axios api

---
+++ client/src/api/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import eventBus from "@/eventBus"
+
+const api = axios.create({
+  baseURL: "/api"
+})
+
+api.interceptors.response.use(null, function (error) {
+  if (error.response) {
+    console.log(error.response)
+    eventBus.emit("axios-error", error.response?.data || "בעיה כללית")
+  }
+
+  return Promise.reject(error)
+})
+
+export default api
+
