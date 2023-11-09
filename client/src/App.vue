b0d9d2895b13bf0dde7dc25d0fff69740391bc34
bokoness <bokoness@gmail.com>
2023-11-09 23:27:19 +0200

    style(client-core): add event buss

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <RouterView/>
+  <div>
+    <RouterView/>
+    <VSnackbar v-model="snackbar" color="red">{{ errorMessage }}</VSnackbar>
+  </div>
 </template>
 
 <script setup>
-//
+import { ref } from "vue"
+import eventBus from "@/eventBus"
+
+const errorMessage = ref("")
+const snackbar = ref(false)
+
+eventBus.on("axios-error", message => {
+  errorMessage.value = message
+  snackbar.value = true
+})
 </script>
