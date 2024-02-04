7ef6bd3772366ac5c67d22123d735302f866560d
Aviel Lanzman <avielnzmn@gmail.com>
2024-02-04 16:53:17 +0200

    add: if user is logged in, replace the login button with the user's avatar

--- client/src/layouts/default/AppBar.vue
+++ client/src/layouts/default/AppBar.vue
@@ -6,20 +6,28 @@
       </VCol>
 
       <VCol lg="4" md="5 " cols="7">
-       <VRow align="center">
-         <VCol>קהילות</VCol>
-         <VCol>אודות</VCol>
-         <VCol>צור קשר</VCol>
-         <VCol>
-           <router-link to="/login">
-             <VBtn color="indigo">
-               <VIcon>mdi-account</VIcon>
-               <span>התחברו</span>
-             </VBtn>
-           </router-link>
-         </VCol>
-       </VRow>
+        <VRow align="center">
+          <VCol>קהילות</VCol>
+          <VCol>אודות</VCol>
+          <VCol>צור קשר</VCol>
+          <VCol>
+            <v-avatar v-if="isLoggedIn" color="info">
+              <v-icon icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+            <router-link v-else to="/login">
+              <VBtn color="indigo">
+                <VIcon>mdi-account</VIcon>
+                <span>התחברו</span>
+              </VBtn>
+            </router-link>
+          </VCol>
+        </VRow>
       </VCol>
     </VRow>
   </VAppBar>
 </template>
+<script setup>
+import { useAuthStore } from "@/store/auth";
+
+const { isLoggedIn } = useAuthStore();
+</script>
