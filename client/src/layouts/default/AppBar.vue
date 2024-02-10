4ea274760d8ad204e091fcbb63906795b4322352
bokoness <bokoness@gmail.com>
2024-02-10 22:21:56 +0200

    chore(appbar): remove the login icon when authenticated

--- client/src/layouts/default/AppBar.vue
+++ client/src/layouts/default/AppBar.vue
@@ -2,7 +2,7 @@
   <VAppBar flat color="secondaryBG" class="pl-15">
     <VRow align="center" justify="space-between" class="px-3">
       <VCol cols="3">
-        <VImg src="icon.png" max-height="80" max-width="=250"/>
+        <VImg src="icon.png" max-height="80" max-width="=250" />
       </VCol>
 
       <VCol lg="4" md="5 " cols="7">
@@ -10,7 +10,7 @@
           <VCol>קהילות</VCol>
           <VCol>אודות</VCol>
           <VCol>צור קשר</VCol>
-          <VCol>
+          <VCol v-if="!isAuthenticated">
             <VBtn color="indigo" @click="openDialog">
               <VIcon>mdi-account</VIcon>
               <span>התחברו</span>
@@ -20,7 +20,7 @@
       </VCol>
     </VRow>
     <VDialog v-model="dialogModel" max-width="600" persistent>
-      <AuthDialog @close-dialog="closeDialog"/>
+      <AuthDialog @close-dialog="closeDialog" />
     </VDialog>
   </VAppBar>
 </template>
@@ -28,8 +28,18 @@
 <script setup>
 import AuthDialog from "@/components/authentication/AuthDialog.vue"
 import { ref } from "vue"
+import { useAuthStore } from "@/store/auth"
+import { onMounted } from "vue";
+
+const auth = useAuthStore()
 
 const dialogModel = ref(false)
+
+const isAuthenticated = ref(false)
+
+onMounted(async () => {
+  isAuthenticated.value = await auth.isAuthenticated()
+})
 
 const openDialog = () => {
   dialogModel.value = true
